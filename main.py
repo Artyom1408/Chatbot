1f90d444b28629932dfee0ec9c5a540eabd1dc12
Artyom1408 <31608847+Artyom1408@users.noreply.github.com>
2019-04-13 18:33:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+
 from nltk import SnowballStemmer
 
 headers = {
@@ -7,22 +8,69 @@ headers = {
 
 def SummaryByName():
     print('Enter name please')
-    r = requests.get("https://api-v3.igdb.com/games/?search="+str(input())+"&fields=name,summary", headers = headers)
-    r = r.json()
+    r = requests.get("https://api-v3.igdb.com/games/?search="+str(input())+"&fields=name,summary", headers = headers).json()
     for i in r:
         print(i.get('name')+'\n')
         print(i.get('summary')+'\n\n')
     return
 
 def NameBySummary():
-    print('Enter genre')
-    r = requests.get("https://api-v3.igdb.com/genres/", headers = headers).json()
-    #TODO
+    genre,company,description,SearchQuoteIn,check,SearchQuote,platform = str(),str(),str(),str(),False,str(),str()
+    genres = [
+        'point-and-click',
+        'fighting',
+        'shooter',
+        'music',
+        'platform',
+        'puzzle',
+        'racing',
+        'real-time-strategy-rts',
+        'role-playing-rpg',
+        'simulator',
+        'sport',
+        'strategy',
+        'turn-based-strategy-tbs',
+        'tactical',
+        'hack-and-slash-beat-em-up',
+        'quiz-trivia',
+        'pinball',
+        'adventure',
+        'indie',
+        'arcade'
+    ]
+    print('Which parameters of game do you know?\n(Such as genre, company (developer or publisher),platform or short description)')
+    ch = SnowballStemmer('english').stem(str(input()))
+    if SnowballStemmer('english').stem('genre') in ch:
+        while check == False:
+            print('Enter genre:')
+            genre = str(input())
+            for k in genres:
+                if SnowballStemmer('english').stem(k) in SnowballStemmer('english').stem(genre):
+                    SearchQuoteIn = "https://api-v3.igdb.com/genres/"+k
+                    check = True
+            if SearchQuote == None:
+                print('I do not know this genre. Repeat pls.')
+    else:
+        SearchQuoteIn = "https://api-v3.igdb.com/"
+    if SnowballStemmer('english').stem('company') in ch:
+        print('Enter company:')
+        company = str(input())                                  #TODO
+        if  SearchQuoteIn == "https://api-v3.igdb.com/"
+        SearchQuote += ',company'
+    if SnowballStemmer('english').stem('platform') in ch:
+        print('Enter platform:')
+        platform = str(input())
+        SearchQuote += ',platform'
+    if SnowballStemmer('english').stem('description') in ch:
+        print('Enter description:')
+        description = str(input())
+        SearchQuote += ',description'
+    
     return
 
 ch = str()  #input var
 check = False  #check var, used in algoryphm cycling
-print('Hello! How can I help you?\nChoose one of these: "find game" or "find summary.')
+print('Hello!\nI can search for specific game or show you summary of any game.\nChoose please.')
 while check == False:
     ch = SnowballStemmer('english').stem(str(input()))
     if SnowballStemmer('english').stem('summary') in ch:
